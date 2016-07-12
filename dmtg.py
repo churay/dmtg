6a637179c5df5d5e6e4440805d24348e509d3cb8
Joseph Ciurej <churayy@gmail.com>
2016-07-11 21:15:38 -0700

    Implemented basic Python library structure for the project.

---
+++ dmtg.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+__doc__ = '''Module for DMTG (Draft Magic the Gathering) Console Interface'''
+
+import os, optparse
+import dmtg
+
+### Main Entry Point ###
+
+def main():
+    print dmtg.fetch_set('hello')
+
+### Miscellaneous ###
+
+if __name__ == '__main__':
+    main()
---
+++ dmtg/mtg.py
@@ -0,0 +1,14 @@
+__doc__ = '''Module for Magic the Gathering Fetcher/Processor Functions'''
+
+import urllib2
+
+### Module Functions ###
+
+def fetch_set(setname):
+    '''Returns a list containing all of the card names in the given set.
+    If a set with a matching name cannot be found, an empty list is returned.'''
+    return []
+
+### Module Helpers ###
+
+
